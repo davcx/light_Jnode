448a734e0a59b32819fc452a0f6e1498629eb08c
DCaredda <firew281-ht@yahoo.it>
2014-04-01 09:17:46 +0200

    dichiarato NLEDS corretto bug su config.nodeId comprensivo di band salvataggio di un solo byte in EEPROM

--- light_Jnode.ino
+++ light_Jnode.ino
@@ -1,7 +1,7 @@
 // Control some LED strips, using settings received by wireless
 // or local button
 // Evoluzione di luceChecco e LuceMarta
-// $Id: light_Jnode.ino 1 2014-3-26 21:21:22Z davide $
+// $Id: light_Jnode.ino 1 2014-3-31 21:21:00Z davide $
 // V2.0
 
 // Source originario per pulsanti: arblink.pde
@@ -43,7 +43,7 @@ funzione		__R		__G		__B		__W
 
 #define DEBUG 1
 
-#define VERSION "\n[light_Jnode.V3.1]"           // keep in sync with the above
+#define VERSION "\n[light_Jnode.V3.2]"           // keep in sync with the above
 
 
 // Parametri radio RF12
@@ -60,12 +60,14 @@ funzione		__R		__G		__B		__W
 
 // NVALUES definisce il numero di valori scambiati su rlink
 #define NVALUES  5        // num campi (to be adjusted from 0..8) (must also adjust "masks")
+
 #define S_DATA   0        // indirizzo del nodo mittente (Sender)
 #define L_DATA   1        // offset in Settings level light
 #define C_DATA   2        // offset command
 #define TMDATA   3        // MSB temperatura trasmessa
 #define TLDATA   4        // LSB temperatura trasmessa
 
+#define NLEDS 4         // numero totale di pwm da pilotare
 #define LOUTR 3		// PD3 IRQ
 #define LOUTG 5		// PD5 P2D
 #define LOUTB 6		// PD6 P3D
@@ -76,7 +78,7 @@ funzione		__R		__G		__B		__W
 #define loButton A0 //lowerbutton
 #define upButton 4  //upperbutton
 #define STEP 1
-#define MAXSTEPS 6
+#define MAXSTEPS 7
 
 #define THERMOM A2 
 //init the one wire interface on AIO Port4
@@ -97,10 +99,10 @@ static word ledState;
 
 static byte settings[NVALUES];	// 0=nIDSender, 1=vIntensita luce, 2=comando ,3=MSB temp, 4=LSB temp
 static byte intensita[NVALUES];	// 0 = red, 1 = green, 2 = blue, 3 = white
-static byte lookUpTbl[4][7] = {
-                      {255,255,250,240,200,100,0},
-                      {255,255,250,240,200,100,0},
-                      {255,255,250,240,200,100,0},
+static byte lookUpTbl[NLEDS][MAXSTEPS] = {
+                      {255,255,250,240,190,100,0},
+                      {255,255,250,230,200,100,0},
+                      {255,255,240,240,200,100,0},
                       {255,254,250,240,200,100,0}
 };
 MilliTimer timer,timerLhouse;
@@ -124,10 +126,10 @@ static void setupIO() {
     
     //upperButton input mode, with pull-up resistor
     pin=upButton;
-    pinMode(pin, INPUT); digitalWrite(pin, 1);
+    pinMode(pin, INPUT); digitalWrite(pin, HIGH);
     //lowerButton input mode, with pull-up resistor
     pin=loButton;
-    pinMode(pin, INPUT); digitalWrite(pin, 1);
+    pinMode(pin, INPUT); digitalWrite(pin, HIGH);
     
 
 //set pin for output
@@ -156,16 +158,18 @@ static void loadConfig() {
         config.nodeId = eeprom_read_byte(RF12_EEPROM_ADDR + 0);
         //((byte*) &config)[i] = eeprom_read_byte(RF12_EEPROM_ADDR + 1);
         config.group = eeprom_read_byte(RF12_EEPROM_ADDR + 1);
-        config.band = config.nodeId & 0xE0;
+        config.band = (config.nodeId & 0xE0)>>6;
         config.lnodeId = config.nodeId & 0x1F;
         config.dnodeId = OUTDest;
 }
 
+
+// Show configurazione RF12 da EEPROM 
 static void showConfig() {
     Serial.println(VERSION);
-    Serial.print("Radio Init->");Serial.print(" ");Serial.print( config.nodeId,HEX);
-    Serial.print("NODEID ");Serial.print( config.nodeId,HEX);Serial.print(" | ");
-    Serial.print("BAND ");Serial.print( config.nodeId & 0xE0,HEX);Serial.print(" | ");
+    Serial.print("Radio Init->");Serial.print(" ");Serial.print( config.nodeId,HEX);Serial.print(" ");
+    Serial.print("NODEID ");Serial.print( config.lnodeId,HEX);Serial.print(" | ");
+    Serial.print("BAND ");Serial.print( (config.nodeId & 0xE0)>>6,HEX);Serial.print(" | ");
     Serial.print("GROUP ");Serial.print( config.group);Serial.print(" | ");
     Serial.println();
     Serial.println();
@@ -173,15 +177,17 @@ static void showConfig() {
 
 // lettura da EEPROM a vettore settings[]
 static void loadSettings() {
-    for (byte i = 0; i < NVALUES; ++i){
-        settings[i] = eeprom_read_byte((byte*) i);
-    }
+//    for (byte i = 0; i < NVALUES; ++i){
+//        settings[i] = eeprom_read_byte((byte*) i);
+//    }
+//load del solo valore utile (Level_Data)
+    settings[L_DATA] = eeprom_read_byte((byte*) L_DATA);
     prepareSlots();
 }
 
 // calcolo vettore stato PWM
 static void prepareSlots() {    
-    for (byte i=0; i < NVALUES; i++){
+    for (byte i=0; i < NLEDS; i++){
         intensita[i] = (lookUpTbl[i][settings[L_DATA]]  ) ;
         analogWrite(masks[i],intensita[i]);
     }
@@ -195,9 +201,11 @@ static void prepareSlots() {
 	    Serial.print(settings[i], DEC);
 	}
 	Serial.println();
+	Serial.println();
 
 	Serial.print("Intensita[] values:");
-	for (byte i = 0; i < NVALUES; ++i) {
+        Serial.println();
+	for (byte i = 0; i < NLEDS; ++i) {
 	    Serial.print(" | ");
 	    Serial.print(i);
 	    Serial.print(" - ");
@@ -210,99 +218,21 @@ static void prepareSlots() {
 
 // scrittura da vettore a EEPROM
 static void saveSettings() {
-    for (byte i = 0; i < NVALUES; ++i){
-	eeprom_write_byte((byte*) i, settings[i]);
-    }
+//    for (byte i = 0; i < NVALUES; ++i){
+//	eeprom_write_byte((byte*) i, settings[i]);
+//    }
+	eeprom_write_byte((byte*) L_DATA, settings[L_DATA]);
     prepareSlots();
 }
 
 
 
-float getTemp12(OneWire  ds,byte datatemp[] ) {
-  byte i;
-  byte present = 0;
-  byte type_s;
-  byte data[12];
-  byte addr[8];
-  float celsius, fahrenheit;
-
-  if ( !ds.search(addr)) {
-    Serial.println("No more addresses.");
-    Serial.println();
-    ds.reset_search();
-    delay(250);
-    return 0;
-  }
-
-  if (OneWire::crc8(addr, 7) != addr[7]) {
-    Serial.println("CRC is not valid!");
-    return 0;
-  }
-  Serial.println();
-
-  // the first ROM byte indicates which chip
-  switch (addr[0]) {
-    case 0x10:
-      Serial.println("  Chip = DS18S20");  // or old DS1820
-      type_s = 1;
-      break;
-    case 0x28:
-      Serial.println("  Chip = DS18B20");
-      type_s = 0;
-      break;
-    case 0x22:
-      Serial.println("  Chip = DS1822");
-      type_s = 0;
-      break;
-    default:
-      Serial.println("Device is not a DS18x20 family device.");
-      return 0 ;
-  }
-
-  ds.reset();
-  ds.select(addr);
-  ds.write(0x44, 1);        // start conversion, with parasite power on at the end
-
-  delay(500);     // maybe 750ms is enough, maybe not
-  // we might do a ds.depower() here, but the reset will take care of it.
-
-  present = ds.reset();
-  ds.select(addr);
-  ds.write(0xBE);         // Read Scratchpad
-
-  for ( i = 0; i < 9; i++) {           // we need 9 bytes
-    data[i] = ds.read();
-  }
-
-  // convert the data to actual temperature
-
-  unsigned int raw = (data[1] << 8) | data[0];
-  if (type_s) {
-    raw = raw << 3; // 9 bit resolution default
-    if (data[7] == 0x10) {
-      // count remain gives full 12 bit resolution
-      raw = (raw & 0xFFF0) + 12 - data[6];
-    }
-  } else {
-    byte cfg = (data[4] & 0x60);
-    if (cfg == 0x00) raw = raw << 3;  // 9 bit resolution, 93.75 ms
-    else if (cfg == 0x20) raw = raw << 2; // 10 bit res, 187.5 ms
-    else if (cfg == 0x40) raw = raw << 1; // 11 bit res, 375 ms
-    // default is 12 bit resolution, 750 ms conversion time
-  }
-  float TemperatureSum = (float)raw / 16.0;
-  Serial.println(TemperatureSum);
-  datatemp[TMDATA] = (int)TemperatureSum;
-  datatemp[TLDATA] = (TemperatureSum - datatemp[TMDATA])*100;
-
-  return TemperatureSum;
-}
 
 
 //********** Setup ***********************
 void setup () {
     Serial.begin(57600);
-    if (rf12_configSilent()) {
+    if (rf12_configSilent()) {   // Se RF12Config restituisce il nodeId
         loadConfig();            // load da EEPROM configurazione RF12
         showConfig();
         settings[S_DATA]=config.lnodeId;
@@ -321,7 +251,7 @@ void loop () {
     delay(200);
     
     //trasmissione faro ogni 10 sec
-    if (timerLhouse.poll(10000)){
+    if (timerLhouse.poll(60000)){
         settings[C_DATA] = 'T';
         float temper = getTemp12(ow,settings );
         Serial.print ("temperatura letta -- ");
@@ -331,10 +261,10 @@ void loop () {
             
     // Se push upperbutton
     if (upperButton() & !pending){
-        if (settings[L_DATA] >= MAXSTEPS){
-            settings[L_DATA]= MAXSTEPS;
-        }else{
+        if (settings[L_DATA] < MAXSTEPS-1){
             settings[L_DATA] += STEP;
+        }else{
+            settings[L_DATA] = (MAXSTEPS - 1);
         }
         Serial.println("UP ->");
         settings[C_DATA] = 'L';
@@ -360,27 +290,29 @@ void loop () {
 // se ricevuto un frame remoto aggiorna il vettore 
 // eventualmente invia ack
     if ( rf12_recvDone()){
-        if( rf12_hdr == (RF12_HDR_DST | config.nodeId)
-		&& rf12_crc == 0
-		&& rf12_len == (NVALUES)
-		&& rf12_data[C_DATA] == 'L') {
-//	    if (RF12_WANTS_ACK && rf12_canSend() ) {
-//	        Serial.println(" -> ack");
-//	        rf12_sendStart(RF12_ACK_REPLY, 0, 0);
-//	    }
-            activityLed (1);
-            Serial.print(" RX frame:     ");
-            for (byte i=0; i < NVALUES; i++){
-                Serial.print(" | ");
-                Serial.print(i);
-                Serial.print(" - ");
-                Serial.println(intensita[i], DEC);
+        if(     rf12_hdr == (RF12_HDR_DST | config.nodeId & 0x1F)
+             && rf12_crc == 0
+	     && rf12_len == NVALUES){
+            if (   rf12_data[S_DATA] == config.lnodeId
+                && rf12_data[C_DATA] == 'L'){
+//	        if (RF12_WANTS_ACK && rf12_canSend() ) {
+//	            Serial.println(" -> ack");
+//	            rf12_sendStart(RF12_ACK_REPLY, 0, 0);
+//	        }
+                activityLed (1);
+                Serial.print(" RX frame:     ");
+                for (byte i=0; i < NVALUES; i++){
+                    Serial.print(" | ");
+                    Serial.print(i);
+                    Serial.print(" - ");
+                    Serial.println(intensita[i], DEC);
+                }
+                //pending = true;
+                memcpy(settings, (void*) rf12_data, rf12_len);
+                saveSettings();
+                delay(100);
+                activityLed (0);
             }
-            //pending = true;
-            memcpy(settings, (void*) rf12_data, rf12_len);
-            saveSettings();
-            delay(100);
-            activityLed (0);
         }
     }
     
@@ -398,7 +330,7 @@ void loop () {
         if ( rf12_canSend()){
             Serial.println("   TX >>>>");
             activityLed (1);
-            rf12_sendStart(RF12_HDR_CTL | RF12_HDR_DST | OUTDest, settings, sizeof(settings));
+            rf12_sendStart(RF12_HDR_CTL | RF12_HDR_DST | config.dnodeId, settings, sizeof(settings));
             delay(100);
             activityLed (0);
             pending = false;
---
+++ miefunzioni.ino
@@ -0,0 +1,80 @@
+float getTemp12 ( OneWire  ds,uint8_t datatemp[] ) {
+    byte i;
+    byte present = 0;
+    byte type_s;
+    byte data[12];
+    byte addr[8];
+    float celsius, fahrenheit;
+    
+    if ( !ds.search(addr)) {
+        Serial.println("No more addresses.");
+        Serial.println();
+        ds.reset_search();
+        delay(250);
+        return 0;
+    }
+    
+    if (OneWire::crc8(addr, 7) != addr[7]) {
+        Serial.println("CRC is not valid!");
+        return 0;
+    }
+    Serial.println();
+    
+    // the first ROM byte indicates which chip
+    switch (addr[0]) {
+        case 0x10:
+            Serial.println("  Chip = DS18S20");  // or old DS1820
+            type_s = 1;
+            break;
+        case 0x28:
+            Serial.println("  Chip = DS18B20");
+            type_s = 0;
+            break;
+        case 0x22:
+            Serial.println("  Chip = DS1822");
+            type_s = 0;
+            break;
+        default:
+            Serial.println("Device is not a DS18x20 family device.");
+            return 0 ;
+    }
+    
+    ds.reset();
+    ds.select(addr);
+    ds.write(0x44, 1);        // start conversion, with parasite power on at the end
+    
+    delay(500);     // maybe 750ms is enough, maybe not
+    // we might do a ds.depower() here, but the reset will take care of it.
+    
+    present = ds.reset();
+    ds.select(addr);
+    ds.write(0xBE);         // Read Scratchpad
+    
+    for ( i = 0; i < 9; i++) {           // we need 9 bytes
+        data[i] = ds.read();
+    }
+    
+    // convert the data to actual temperature
+    
+    unsigned int raw = (data[1] << 8) | data[0];
+    if (type_s) {
+        raw = raw << 3; // 9 bit resolution default
+        if (data[7] == 0x10) {
+            // count remain gives full 12 bit resolution
+            raw = (raw & 0xFFF0) + 12 - data[6];
+        }
+    } else {
+        byte cfg = (data[4] & 0x60);
+        if (cfg == 0x00) raw = raw << 3;  // 9 bit resolution, 93.75 ms
+        else if (cfg == 0x20) raw = raw << 2; // 10 bit res, 187.5 ms
+        else if (cfg == 0x40) raw = raw << 1; // 11 bit res, 375 ms
+        // default is 12 bit resolution, 750 ms conversion time
+    }
+    float TemperatureSum = (float)raw / 16.0;
+    Serial.println(TemperatureSum);
+    datatemp[TMDATA] = (int)TemperatureSum;
+    datatemp[TLDATA] = (TemperatureSum - datatemp[TMDATA])*100;
+    
+    return TemperatureSum;
+}
+
